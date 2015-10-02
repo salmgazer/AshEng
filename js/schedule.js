a442d1d26190141a1cb1ff56ecaf2128856d2196
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-10-02 03:51:28 +0000

    Stopping for the night and will continue later. The popup for profile pictures are currently working.

---
+++ js/schedule.js
@@ -0,0 +1,9 @@
+/*global $, alert*/
+
+$(function () {
+    'use strict';
+    $('#profile_pic').click(function () {
+        alert('Image pressed');
+        $('#modal1').openModal();
+    });
+});
