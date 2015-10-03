6d910364f99ce2a9536dead26ca8398b37db6a82
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-10-03 11:17:06 +0000

    More modifications

--- js/schedule.js
+++ js/schedule.js
@@ -9,28 +9,46 @@ $(function () {
         alert("Scan button pressed");
         cordova.plugins.barcodeScanner.scan(
             function (result) {
+
                 $("#barcode_result").val(result.text);
+
+                $("#electronics_barcode").hide("fast");
+                    $("#mechanical_barcode").hide("fast");
+                    $("#science_barcode").hide("fast");
+                    $("#design_barcode").hide("fast");
 
                 var response = result.text;
 
-                if (response == 'skip_barcode') {
+                if (response == 'electronics_barcode') {
                     $("#scan_ui").hide("fast");
-                    $("#skip_barcode").show("fast");
+                    $("#design_barcode").hide("fast");
+                    $("#science_barcode").hide("fast");
+                    $("#mechanical_barcode").show("fast");
+                    $("#electronics_barcode").show("fast");
+                }
+
+                if (response == 'mechanical_barcode') {
+                    $("#scan_ui").hide("fast");
+                    $("#electronics_barcode").hide("fast");
+                    $("#design_barcode").hide("fast");
+                    $("#science_barcode").hide("fast");
+                    $("#mechanical_barcode").show("fast");
+                }
+
+                if (response == 'science_barcode') {
+                    $("#scan_ui").hide("fast");
+                    $("#electronics_barcode").hide("fast");
+                    $("#mechanical_barcode").hide("fast");
+                    $("#design_barcode").hide("fast");
+                    $("#science_barcode").show("fast");
                 }
 
                 if (response == 'design_barcode') {
                     $("#scan_ui").hide("fast");
+                     $("#electronics_barcode").hide("fast");
+                    $("#mechanical_barcode").hide("fast");
+                    $("#science_barcode").hide("fast");
                     $("#design_barcode").show("fast");
-                }
-
-                if (response == 'databank_barcode') {
-                    $("#scan_ui").hide("fast");
-                    $("#databank_barcode").show("fast");
-                }
-
-                if (response == 'norton_barcode') {
-                    $("#scan_ui").hide("fast");
-                    $("#norton_barcode").show("fast");
                 }
 
             },
@@ -39,6 +57,8 @@ $(function () {
             }
         );
     });
+
+
 });
 
 
