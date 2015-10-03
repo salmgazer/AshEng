a9ad1b94ce1416eeb4d32a0dea7f20c2aa06ad9d
Salifu Mutaru <salifumutaru@gmail.com>
2015-10-03 03:59:15 +0000

    footer, button, white header

--- js/valid.js
+++ js/valid.js
@@ -3,20 +3,16 @@ var check_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2
 var check_phone =  /^[0-9+]{10,15}$/;
 var check_occupation = /^[A-Za-z ]{4,50}$/;
 var check_workplace = /^[A-Za-z0-9-' ]{2,50}$/;
-//var check_nationality = /^[A-Za-z-' ]{4,50}$/;
+var check_nationality = /^[A-Za-z-' ]{4,50}$/;
 var errors = [];
 
 function validate(){
-var nationality = document.getElementById('nationality').options[document.getElementById('nationality').selectedIndex].text;
-
+var nationality = document.getElementById('nationality').value;
 var name = document.getElementById('fn').value;
 var email = document.getElementById('email').value;
 var phone = document.getElementById('thephone').value;
 var occupation = document.getElementById('occupation').value;
 var workplace = document.getElementById('workplace').value;
-if(nationality == "Select your country"){
-    errors[errors.length] = "Please select your country";
-}
 
  if (!check_name.test(name)) {
   errors[errors.length] = "Enter valid name .";
@@ -33,9 +29,9 @@ if(nationality == "Select your country"){
  if (!check_workplace.test(workplace)) {
   errors[errors.length] = "Enter valid workplace ";
  }
- /*if (!check_nationality.test(nationality)) {
-  errors[errors.length] = "You must enter a valid country ";
- }*/
+ if (!check_nationality.test(nationality)) {
+  errors[errors.length] = "Enter your country ";
+ }
  if (errors.length > 0) {
 
   reportErrors(errors);
@@ -49,8 +45,9 @@ function reportErrors(errors){
  var msg = ""
 ; for (var i = 0; i<errors.length; i++) {
  var numError = i + 1;
- msg += '<div class="chip red white-text mychip">'+errors[i]+'<i class="material-icons white-text">close</i></div>';
+ msg += '<div class="chip teal-text mychip">'+errors[i]+'<i class="material-icons teal-text">close</i></div>';
 }
 console.log(errors);
 document.getElementById("myerrors").innerHTML = msg;
+window.location.href = "#topper";
 }
